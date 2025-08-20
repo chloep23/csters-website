9a0e9ca80ab28c7d9fe7079509fe5c40da7de727
Richa Jayanti <rj63@gmail.com>
2025-08-19 21:42:53 -0700

    WIP: edits to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -40,7 +40,7 @@ export default function Home() {
           {/* Text */}
           <div className="w-full md:w-1/2 p-6 md:p-12 z-10 text-center md:text-left">
             {/* Instead of 30 px like the figma layout, i made it 25 so it would fit in one line. */}
-            <p className="text-[25px] font-normal text-[#0B1956] text-center pl-8">
+            <p className="text-[30px] font-normal text-[#0B1956] text-center 8-23">
               Follow our{" "}
               <a
                 href="https://www.instagram.com/ricecsters/"
@@ -60,14 +60,44 @@ export default function Home() {
             <img
               src="/images/homepage/instagram.png"
               alt="Instagram pic"
-              className="w-[600px] h-[600px] flex-shrink-0 aspect-square translate-y-20 translate-x-20"
+              className="w-[760px] h-[760px] flex-shrink-0 aspect-square translate-y-25 translate-x-25"
             />
           </div>
         </div>
       </section>
 
-
-
+      <section className="relative w-full h-[795px] bg-[#F7F4F1] overflow-visible">
+        {/* Cloud background SVG */}
+        <svg
+          width="1440"
+          height="795"
+          viewBox="0 0 1440 795"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute top-0 left-0 w-full h-full -z-0 -translate-y-30"
+        >
+          <ellipse cx="45.5" cy="493.5" rx="430.5" ry="493.5" fill="#8D99C6" />
+          <ellipse cx="1156.5" cy="674.5" rx="298.5" ry="344.5" fill="#8D99C6" />
+          <ellipse cx="479" cy="233" rx="241" ry="233" fill="#8D99C6" />
+          <ellipse
+            cx="685.019"
+            cy="570.427"
+            rx="348.521"
+            ry="249.891"
+            transform="rotate(-49.6914 685.019 570.427)"
+            fill="#8D99C6"
+          />
+          <ellipse
+            cx="1550.49"
+            cy="705.152"
+            rx="490.5"
+            ry="435.5"
+            transform="rotate(143.617 1550.49 705.152)"
+            fill="#8D99C6"
+          />
+          
+        </svg>
+      </section>
     </div>
   );
 }
